9185f5213d5a1ca2ed5adbbd45852e78eee68baf
AtithiShekhar <atithishekhar@gmail.com>
2025-02-11 22:46:26 +0530

    Create About.jsx

---
+++ About.jsx
@@ -0,0 +1,86 @@
+/**
+ * About component
+ *
+ * Space for you to describe more about yourself.
+ */
+
+import React from "react";
+
+/**
+ * About background image
+ *
+ * Below is a sample image. Upload the image of your choice into the "images"
+ * directory and import here for use. Then, set imageAltText to string that 
+ * represents what you see in that image.
+ *
+ * Need an image? Check out https://unsplash.com to download a image you
+ * freely use on your site.
+ */
+import image from "../images/motion-background.jpg";
+
+const imageAltText = "purple and blue abstract background";
+
+/**
+ * Sort description that expands on your title on the Home component.
+ */
+const description =
+  "I am a student of Jabalpur Engineering collage , Jabalpur , currently persuaing BTech in Computer Science and Engineering and along with the academics , i am focusing on app development for android and also learning about web development too . I am also intrested in DSA and curently active on leetcode.";
+
+/**
+ * List of some of skills or technologies you work on, are learning,
+ * passionate about, or enjoy,
+ */
+const skillsList = [
+  "App Developer",
+  "Decent Projects",
+  "Inclusive design",
+  "Focus group testing",
+  "Android Application Build",
+  "LeetCode Problems solve",
+];
+
+/**
+ * Use this to give more information about what you are passionate about,
+ * how you best work, or even a quote. This will help someone learn more
+ * about you on a professional level.
+ */
+const detailOrQuote =
+  "I am currently focusing on the field of app development passionately and also along with that i am also learning little bit of web development";
+
+const About = () => {
+  return (
+    <section className="padding" id="about">
+      <img className="background" src={image} alt={imageAltText} />
+      <div
+        style={{
+          backgroundColor: "white",
+          width: "50%",
+          padding: "4rem",
+          margin: "3rem auto",
+          textAlign: "center",
+        }}
+      >
+        <h2>About Myself</h2>
+        <p className="large">{description}</p>
+        <hr />
+        <ul
+          style={{
+            textAlign: "left",
+            columns: 2,
+            fontSize: "1.25rem",
+            margin: "2rem 3rem",
+            gap: "3rem",
+          }}
+        >
+          {skillsList.map((skill) => (
+            <li key={skill}>{skill}</li>
+          ))}
+        </ul>
+        <hr />
+        <p style={{ padding: "1rem 3rem 0" }}>{detailOrQuote}</p>
+      </div>
+    </section>
+  );
+};
+
+export default About;
